ee10c27c5ec08bdaa85077e505abd117a64bfa34
simrandhankar <simran.dhankar@itmtb.com>
2023-06-19 18:10:29 +0530

    changes

--- controllers/productController.js
+++ controllers/productController.js
@@ -220,13 +220,13 @@ export const productCountController = async (req, res) => {
 //product list based on page
 export const productListController = async (req, res) => {
   try {
-    const perPage = 3;
+    const perPage = 4;
     const page = req.params.page ? req.params.page : 1;
     const products = await productModel
       .find({})
       .select("-photo")
       .skip((page - 1) * perPage)
-      .limit(perPage)
+      // .limit(perPage)
       .sort({ createdAt: -1 });
     res.status(200).send({
       success: true,
